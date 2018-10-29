dce78033573dbb3512cf5525f7ef0841894cbe18
sojason <jakob.sonnerstam@gmail.com>
2018-10-29 10:47:56 +0100

    Created Datastore

---
+++ datastore.h
@@ -0,0 +1,38 @@
+#ifndef DATASTORE_H
+#define DATASTORE_H
+
+#include <sqlite3.h>
+
+// Datastore will store file metadata
+// Database table layout
+// Where is the file now? | When was image/video created? | width of img? | height of img? | length of video? | Where should it go?
+// filepath | type | creation_date | width | height | length | destination_path
+
+enum FileType {
+  OTHER = 0
+  IMAGE = 1,
+  VIDEO = 2,
+  PDF   = 3,
+  DOC   = 4,
+  TEXT  = 5
+};
+
+class Datastore
+{
+  public: 
+    Datastore();
+    ~Datastore();
+    
+    void set_path(std::string p)  { path_to_db = p; }
+    void set_name(std::string n)  { name_of_db = n; }
+    bool open();                    ///<  Open, and initiate a new database.
+    bool open(std::string db_path); ///< Open an existing database, RO?
+    void set(std::string path, FileType ft, std::string date, int w, int h, double dur, std::string dest);
+    std::vector<std::string>  dump(); ///< Return all entries in db
+    
+  private:
+    std::string path_to_db;
+    sqlite3*  db;
+};
+
+#endif
