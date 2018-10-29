7b743c9c3640d8d63209cfd5f798852449c5987e
sojason <jakob.sonnerstam@gmail.com>
2018-10-29 11:29:09 +0100

    Update datastore.h

--- datastore.h
+++ datastore.h
@@ -27,11 +27,16 @@ class Datastore
     void set_name(std::string n)  { name_of_db = n; }
     bool open();                    ///<  Open, and initiate a new database.
     bool open(std::string db_path); ///< Open an existing database, RO?
-    void set(std::string path, FileType ft, std::string date, int w, int h, double dur, std::string dest);
+    bool set(std::string path, FileType ft, std::string date, int w, int h, double dur, std::string dest);
     std::vector<std::string>  dump(); ///< Return all entries in db
-    
+    std::string get_error() { return error; }  
+  
   private:
+    std::string get_file_path();  // Return full file path based on path and name
     std::string path_to_db;
+    std::string name_of_db;
+    std::string file_path;
+    std::string error;
     sqlite3*  db;
 };
 
